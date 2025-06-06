46169a3760d652102ffc1f72d91e9632585d73e0
Sasha <example@gmail.com>
2025-06-06 21:39:59 +0300

    First practice is completed

--- lesson18-practice1/js/script.js
+++ lesson18-practice1/js/script.js
@@ -2,24 +2,27 @@
 
 // 1)
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 // 2)
 
-const personalMovieDB ={
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false,
+    privat: false
 };
 
 //3
 
-const answerAboutFilm = prompt('Один из последних просмотренных фильмов?');
-const answerEstimation = prompt('На сколько оцените его?');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-const movies = {
-    movie: answerAboutFilm,
-    estimation: answerEstimation,
-};
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
