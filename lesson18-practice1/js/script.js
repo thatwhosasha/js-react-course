2e8db18e82416a730303348dac3d76bc62542a7e
Sasha <example@gmail.com>
2025-06-06 20:45:05 +0300

    Second task is done

--- lesson18-practice1/js/script.js
+++ lesson18-practice1/js/script.js
@@ -1,5 +1,16 @@
-"use strict"
+'use strict';
+
+// 1)
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
 console.log(numberOfFilms);
 
+// 2)
+
+const personalMovieDB ={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
