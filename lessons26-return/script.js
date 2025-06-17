b699822e5db1ee56a2037c3341a7711cb25b67e7
Sasha <example@gmail.com>
2025-06-17 15:01:50 +0300

    lesson 26 is complete

---
+++ lessons26-return/script.js
@@ -0,0 +1,28 @@
+const usdCurr = 28;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    return curr * amount;
+}
+
+function promotion(result) {
+    console.log(result * discount);
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+function test() {
+    for (let i =0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return;
+    }
+    console.log('Done');
+}
+
+test(); // 0 1 2 3
+
+
+function doNothing() {};
+
+console.log(doNothing()); // undefined
