bc73f0a11387962fe198523c76b7bfc0fe9a0516
Sasha <example@gmail.com>
2025-06-17 14:30:23 +0300

    lessons 24,25 is complete

---
+++ lessons24-25-functions/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstMessage('Hello');
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
